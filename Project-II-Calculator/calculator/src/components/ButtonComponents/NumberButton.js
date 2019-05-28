20c898ae8e2c2705c5bfb902c33a3b86d94a905d
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-05-28 13:55:34 -0600

    number button component finished

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return (
+        <div className={props.numberProp.toString()}>
+            {props.numberProp.toString()}
+        </div>
+    )
+}
+
+export default NumberButton;
